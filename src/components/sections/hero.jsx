c037a7f2985b8dd35f7057cdec6cf4487e31f158
Gregory Subero <suberog010@gmail.com>
2026-07-28 20:56:50 -0400

    creacion de carpeta para componentes

---
+++ src/components/sections/hero.jsx
@@ -0,0 +1,18 @@
+
+const Hero = () => {
+    return(
+        <div className="p-5.5">
+            <div>
+              Logo     
+            </div>
+            <div>
+                
+            </div>
+            <section>
+                
+            </section>
+        </div>
+    )
+}
+
+export default Hero
