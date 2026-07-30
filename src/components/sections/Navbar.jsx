d42f6c42a626f7836c041d3237522e1049362e76
Gregory Subero <suberog010@gmail.com>
2026-07-29 22:47:47 -0400

    fix: añadiendo padding top 20 para no tapar el contenido del main

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -3,11 +3,11 @@ import {Menu} from 'lucide-react';
 
 const Navbar = () => {
     return(
-        <nav className="flex justify-between items-center  bg-nav-primary">
-            <div className="text-primary-tx">
+        <nav className="fixed z-50 inset-x-0 top-0 flex justify-between items-center w-full  bg-nav-primary py-5 border-b-[0.5px] border-b-primary-tx/50">
+            <div className="text-primary-tx pl-5">
                 <h2>Gregory Subero. / Dev </h2>
             </div>
-            <div className="text-primary-tx">
+            <div className="text-primary-tx pr-5">
                 <Menu />
             </div>
         </nav>
