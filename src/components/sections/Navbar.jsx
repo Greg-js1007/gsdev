27b6f0713411b79db62f727462c806fada867fa7
Gregory Subero <suberog010@gmail.com>
2026-07-30 21:31:39 -0400

    style: añadir media query de ocultar a los 64 rem

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -3,7 +3,7 @@ import {Menu} from 'lucide-react';
 
 const Navbar = () => {
     return(
-        <nav className="fixed z-50 inset-x-0 top-0 flex justify-between items-center w-full  bg-nav-primary py-5 border-b-[0.5px] border-b-primary-tx/50">
+        <nav className="fixed z-50 inset-x-0 top-0 lg:hidden flex justify-between items-center w-full  bg-nav-primary py-5 border-b-[0.5px] border-b-primary-tx/50">
             <div className="text-primary-tx pl-5">
                 <h2>Gregory Subero. / Dev </h2>
             </div>
