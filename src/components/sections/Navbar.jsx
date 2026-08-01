b977b9a1bce4042ad311fadb4f09f237268b8565
Gregory Subero <suberog010@gmail.com>
2026-08-01 07:47:04 -0400

    feat: renderizar lista de enlaces en el menu mobile

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import {Menu} from 'lucide-react';
+import NavLink from "../ui/NavLink";
 
 const Navbar = () => {
 
@@ -8,6 +9,12 @@ const Navbar = () => {
     const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
     const closeMenu = () => setIsMenuOpen(false);
     
+    const navLinks = [
+        { href: "#", number: "01", text: "Habilidades" },
+        { href: "#", number: "02", text: "Proyectos" },
+        { href: "#", number: "03", text: "Trayectoria" },
+        { href: "#", number: "04", text: "Contacto" },
+    ]
 
     return(
         <nav className="fixed z-50 inset-x-0 top-0 lg:hidden flex justify-between items-center w-full  bg-nav-primary py-5 border-b-[0.5px] border-b-primary-tx/50">
@@ -17,6 +24,15 @@ const Navbar = () => {
             <div className="text-primary-tx pr-5">
                 <Menu />
             </div>
+            {isMenuOpen &&(
+                <div id="menu-mobile" className="flex flex-col gap-y-4" >
+                {navLinks.map((link) => (
+                    <NavLink key={link.number} href={link.href} number={link.number}>
+                        {link.text}
+                    </NavLink>
+                ))}
+            </div>
+            )}
         </nav>
     )
 }
