04e8512b445a0e66f3c99369da32c132fecab7de
Gregory Subero <suberog010@gmail.com>
2026-08-01 07:49:16 -0400

    style: cambiar span color y espaciado al li

--- src/components/ui/NavLink.jsx
+++ src/components/ui/NavLink.jsx
@@ -3,11 +3,11 @@
 const NavLink = ({href, children, number}) => {
     return(
         <ul>
-            <li className="border-b border-primary-tx/50">
-                <span>
+            <li className="border-b border-primary-tx/50 flex items-center gap-2.5">
+                <span className="text-gray-300 text-sm">
                     {number}
                 </span>
-                <a href={href} className="capitalize text-2xl py-2.5 font-semibold">{children}</a>
+                <a href={href} className="capitalize text-2xl py-2.5 font-semibold text-primary-tx transition-all ease-in-out duration-300 hover:text-secundary-tx focus:text-secundary-tx">{children}</a>
             </li>
         </ul>
     )
