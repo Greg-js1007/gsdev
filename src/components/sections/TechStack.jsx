5a01d74ec148d4d4c02481ded8fe7be66f73cb80
Gregory Subero <suberog010@gmail.com>
2026-08-06 22:26:29 -0400

    feat: añadir componente al section y mapear con estilos

---
+++ src/components/sections/TechStack.jsx
@@ -0,0 +1,24 @@
+import CardStack from "../ui/CardStack";
+import { Boxes } from 'lucide-react';
+
+
+
+
+const TechStack = () => {
+    const Stacks = [
+        { title: "React / Next.js", description: "Interfaces declarativas, escalables y de alto rendimiento.", icon: Boxes, label: "Frontend Core" },
+        
+    ]
+    return (
+        <div className="pt-20">
+            <h2 className="text-3xl md:text-4xl font-bold text-center text-secondary text-start"><span className="text-primary-tx">Un stack afilado</span> <br />  para proyectos con carácter.</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-10">
+                {Stacks.map((stack, index) => (
+                    <CardStack key={index} {...stack} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default TechStack
