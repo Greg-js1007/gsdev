48127598ca3c126525429bd28c0f05c5f1e812b2
Gregory Subero <suberog010@gmail.com>
2026-08-06 20:27:06 -0400

    feat: añadiendo icon y estilos

--- src/components/ui/SocialLinks.jsx
+++ src/components/ui/SocialLinks.jsx
@@ -2,8 +2,10 @@
 const SocialLink = ({icon: Icon, url}) => {
     return(
         <ul className="flex">
-            <li className="w-full h-full p-2 rounded-full flex items-center justify-center border border-primary-tx/50 hover:bg-primary-bg transition-all duration-200">
-                <a href={url} target="_blank" rel="noopener noreferrer" ><Icon style={{color: "white",opacity: "10"}} /></a>
+            <li className="w-full h-full p-2 rounded-full flex items-center justify-center border border-primary-tx/50 hover:border-secundary-tx transition-all duration-300 ease-out hover:translate-x-1 hover:translate-y-1">
+                <a href={url} target="_blank" rel="noopener noreferrer" className="cursor-pointer text-primary-tx/50  hover:text-primary-tx" >
+                    <Icon />
+                </a>
             </li>
         </ul>
     )
