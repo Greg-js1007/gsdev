9479f9f9170d1b4f24dc2e2b46b9f8abca4560b8
Gregory Subero <suberog010@gmail.com>
2026-08-07 22:20:22 -0400

    style: añadiir hover de color cyan al icono y al borde circular

--- src/components/ui/SocialLinks.jsx
+++ src/components/ui/SocialLinks.jsx
@@ -3,8 +3,8 @@ const SocialLink = ({icon: Icon, url}) => {
     return(
         <ul className="flex">
             <li className="w-full h-full p-2 rounded-full flex items-center justify-center border border-primary-tx/50 hover:border-secundary-tx transition-all duration-300 ease-out hover:translate-x-1 hover:translate-y-1">
-                <a href={url} target="_blank" rel="noopener noreferrer" className="cursor-pointer text-primary-tx/50  hover:text-primary-tx" >
-                    <Icon />
+                <a href={url} target="_blank" rel="noopener noreferrer" className="cursor-pointer text-primary-tx/50  hover:text-secundary-tx" >
+                    <Icon className="hover:text-secundary-tx" />
                 </a>
             </li>
         </ul>
