924b4dbc59468ee453708007852e007b2c3abfb8
Gregory Subero <suberog010@gmail.com>
2026-08-07 22:21:48 -0400

    style: agregar padding bottom y top

--- src/components/sections/TechStack.jsx
+++ src/components/sections/TechStack.jsx
@@ -10,7 +10,7 @@ const TechStack = () => {
         {title: "Performance", description: "Optimización para experiencias rápidas y fluidas.", icon: Sparkles, label: "Web Vitals", color: "text-icon-card-1"}
     ]
     return (
-        <div className="pt-20">
+        <div className="py-20">
             <h2 className="text-3xl md:text-4xl font-bold text-center text-secondary text-start"><span className="text-primary-tx">Un stack afilado</span> <br />  para proyectos con carácter.</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-10">
                 {Stacks.map((stack, index) => (
