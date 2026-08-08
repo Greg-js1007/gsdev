db821bfaa4e2de7ffc4736deccd00826f2289e34
Gregory Subero <suberog010@gmail.com>
2026-08-07 21:03:48 -0400

    style: añadiendo media queries para desktop

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -8,7 +8,7 @@ const MainLayout  = ({children}) => {
             <header>
                 <Navbar />
             </header>
-            <main className="px-5">
+            <main className="px-5 lg:flex gap-10">
                 <Hero />
                 <TechStack />
             </main>
