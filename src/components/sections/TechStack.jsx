7f8937d0d6135d3b83ed32a704e641b3885f22d9
Gregory Subero <suberog010@gmail.com>
2026-08-09 21:01:34 -0400

    style: cambiando el border-color que se envia el componente

--- src/components/sections/TechStack.jsx
+++ src/components/sections/TechStack.jsx
@@ -3,11 +3,11 @@ import { Boxes, Palette, CodeXml, Sparkles, Database } from 'lucide-react';
 
 const TechStack = () => {
     const Stacks = [
-        { title: "React / Next.js", description: "Interfaces declarativas, escalables y de alto rendimiento.", icon: Boxes, label: "Frontend Core",color: "text-icon-card-1" },
-        {title: "TypeScript", description: "Tipado seguro que reduce bugs y mejora el desarrollo", icon: CodeXml, label: "Code Safe",color: "text-icon-card-2"},
-        {title: "Node.js / Express", description: "Construye APIs rápidas y escalables con entornos robustos.", icon: Database, label: "Backend - APIs", color: "text-icon-card-1"},
-        {title: "Tailwind", description: "Diseño rápido y responsivo con un sistema de clases consistente.", icon: Palette, label: "Styling System", color: "text-icon-card-2"}, 
-        {title: "Performance", description: "Optimización para experiencias rápidas y fluidas.", icon: Sparkles, label: "Web Vitals", color: "text-icon-card-1"}
+        { title: "React / Next.js", description: "Interfaces declarativas, escalables y de alto rendimiento.", icon: Boxes, label: "Frontend Core",color: "text-icon-card-1", borderHover: "hover:border-secundary-tx" },
+        {title: "TypeScript", description: "Tipado seguro que reduce bugs y mejora el desarrollo", icon: CodeXml, label: "Code Safe",color: "text-icon-card-2", borderHover: "hover:border-icon-card-2"},
+        {title: "Node.js / Express", description: "Construye APIs rápidas y escalables con entornos robustos.", icon: Database, label: "Backend - APIs", color: "text-icon-card-1", borderHover: "hover:border-secundary-tx"},
+        {title: "Tailwind", description: "Diseño rápido y responsivo con un sistema de clases consistente.", icon: Palette, label: "Styling System", color: "text-icon-card-2", borderHover: "hover:border-icon-card-2"}, 
+        {title: "Performance", description: "Optimización para experiencias rápidas y fluidas.", icon: Sparkles, label: "Web Vitals", color: "text-icon-card-1", borderHover: "hover:border-secundary-tx"}
     ]
     return (
         <div className="py-20">
