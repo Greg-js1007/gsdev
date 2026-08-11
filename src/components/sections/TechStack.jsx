7d8719dedc923a73ca5589e87adce9c1447f8b3b
Gregory Subero <suberog010@gmail.com>
2026-08-10 20:09:12 -0400

    feat: añadiendo CardSkills y quitando Card

--- src/components/sections/TechStack.jsx
+++ src/components/sections/TechStack.jsx
@@ -1,20 +1,20 @@
-import Cards from "../ui/Cards";
 import { Boxes, Palette, CodeXml, Sparkles, Database } from 'lucide-react';
+import CardSkill from "../ui/CardSkill";
 
 const TechStack = () => {
     const Stacks = [
-        { title: "React / Next.js", description: "Interfaces declarativas, escalables y de alto rendimiento.", icon: Boxes, label: "Frontend Core",color: "text-icon-card-1", borderHover: "hover:border-secundary-tx" },
-        {title: "TypeScript", description: "Tipado seguro que reduce bugs y mejora el desarrollo", icon: CodeXml, label: "Code Safe",color: "text-icon-card-2", borderHover: "hover:border-icon-card-2"},
-        {title: "Node.js / Express", description: "Construye APIs rápidas y escalables con entornos robustos.", icon: Database, label: "Backend - APIs", color: "text-icon-card-1", borderHover: "hover:border-secundary-tx"},
-        {title: "Tailwind", description: "Diseño rápido y responsivo con un sistema de clases consistente.", icon: Palette, label: "Styling System", color: "text-icon-card-2", borderHover: "hover:border-icon-card-2"}, 
-        {title: "Performance", description: "Optimización para experiencias rápidas y fluidas.", icon: Sparkles, label: "Web Vitals", color: "text-icon-card-1", borderHover: "hover:border-secundary-tx"}
+        { id:1, title: "React / Next.js", description: "Interfaces declarativas, escalables y de alto rendimiento.", icon: Boxes, label: "Frontend Core",color: "text-icon-card-1", borderHover: "hover:border-secundary-tx" },
+        { id:2,title: "TypeScript", description: "Tipado seguro que reduce bugs y mejora el desarrollo", icon: CodeXml, label: "Code Safe",color: "text-icon-card-2", borderHover: "hover:border-icon-card-2"},
+        { id:3,title: "Node.js / Express", description: "Construye APIs rápidas y escalables con entornos robustos.", icon: Database, label: "Backend - APIs", color: "text-icon-card-1", borderHover: "hover:border-secundary-tx"},
+        { id:4,title: "Tailwind", description: "Diseño rápido y responsivo con un sistema de clases consistente.", icon: Palette, label: "Styling System", color: "text-icon-card-2", borderHover: "hover:border-icon-card-2"}, 
+        { id:5,title: "Performance", description: "Optimización para experiencias rápidas y fluidas.", icon: Sparkles, label: "Web Vitals", color: "text-icon-card-1", borderHover: "hover:border-secundary-tx"}
     ]
     return (
         <div className="py-20">
             <h2 className="text-3xl md:text-4xl font-bold text-center text-secondary text-start"><span className="text-primary-tx">Un stack afilado</span> <br />  para proyectos con carácter.</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-10">
-                {Stacks.map((stack, index) => (
-                    <Cards key={index} {...stack} />
+                {Stacks.map((stack, id) => (
+                    <CardSkill key={id} {...stack} />
                 ))}
             </div>
         </div>
