806501080eb61a004b92f640c7488eefa4dbd4ad
Gregory Subero <suberog010@gmail.com>
2026-08-10 20:55:00 -0400

    style: eliminando borderHover

--- src/components/ui/Cards.jsx
+++ src/components/ui/Cards.jsx
@@ -1,6 +1,6 @@
 const Cards = ({ children}) => {
     return (
-        <div className={`flex flex-col items-start gap-4 bg-bg-darks/30 rounded-2xl p-5 border border-secondary ${borderHover}`}>
+        <div className={`flex flex-col items-start gap-4 bg-bg-darks/30 rounded-2xl p-5 border border-secondary`}>
             {children}
         </div>
     )
