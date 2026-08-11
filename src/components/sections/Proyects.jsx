39ff9fd4d32f3d1a6d76c1a06078610ac84df435
Gregory Subero <suberog010@gmail.com>
2026-08-10 21:40:45 -0400

    feat: añadiendo nuevo objeto con datos

--- src/components/sections/Proyects.jsx
+++ src/components/sections/Proyects.jsx
@@ -10,16 +10,27 @@ const Projects = () => {
             description: "Plataforma de e-commerce y gestión integrado y personalización de productos.",
             icon: ShoppingBag,
             link: "https://github.com/GSUBERO/SastreriaPremium",
-            skills: ['React', 'Taiwlind', 'Framer']
+            skills: ['React.js', 'Taiwlind', 'Framer']
                
-        }
+        }, 
+        {
+            id: 2, 
+            date: '2025-10',
+            title: "Hotel Gatuno",
+            description: "Pagina web para presentar los servicios de un hotel para gatos",
+            icon: ShoppingBag,
+            link: "https://www.hotelgatuno.com/m",
+            skills: ['Astro', 'Taiwlind', 'React.js']
+               
+        },
+
     ]
     return (
         <div className="py-10">
             <div className="pb-5">
                 <h2 className="text-3xl md:text-4xl font-bold text-center text-primary-tx text-start">Projects</h2>
             </div>
-            <section>
+            <section className="flex flex-col gap-7 w-full">
                 {projects.map((project, id) => (
                     <CardProjects 
                         key={id} 
