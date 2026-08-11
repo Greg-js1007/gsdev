d3e532b212f05c7501f0e1c7c02752614d9bd773
Gregory Subero <suberog010@gmail.com>
2026-08-10 21:28:33 -0400

    feat: condicional para renderizar el h2

--- src/components/ui/Chip.jsx
+++ src/components/ui/Chip.jsx
@@ -2,7 +2,7 @@
 const Chip = ({number = "", text}) => {
     return (
         <div className="rounded-2xl gap-2 items-center border inline-flex px-2 py-1.5 border-secundary-tx bg-blue-900/50">
-            <h2 className="text-[10px] text-secundary-tx">{number}</h2>
+            {number && <h2 className="text-[10px] text-secundary-tx">{number}</h2>}
             <p className="text-[10px] text-secundary-tx text-center">{text}</p>
         </div>
     )
