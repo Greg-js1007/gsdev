db99638ba51801cf1bc0d09bbdd24c8b2c43f13a
Gregory Subero <suberog010@gmail.com>
2026-08-10 21:34:03 -0400

    style: cambiando el border style y fondo

--- src/components/ui/Cards.jsx
+++ src/components/ui/Cards.jsx
@@ -1,6 +1,6 @@
 const Cards = ({ children}) => {
     return (
-        <div className={`flex flex-col items-start gap-4 bg-bg-darks/30 rounded-2xl p-5 border border-secondary`}>
+        <div className={`flex flex-col items-start gap-5 border border-white/10 bg-slate-900/40 backdrop-blur-sm rounded-2xl p-5`}>
             {children}
         </div>
     )
