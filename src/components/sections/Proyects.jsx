d6e725077b4b5457a1c123782f6ae2e37c1b1f51
Gregory Subero <suberog010@gmail.com>
2026-08-10 20:09:44 -0400

    feat: agregando projects array y estructura del componente

--- src/components/sections/Proyects.jsx
+++ src/components/sections/Proyects.jsx
@@ -1,9 +1,29 @@
-
-
+import Cards from "../ui/Cards"
+import { ShoppingBag } from 'lucide-react';
 const Projects = () => {
+
+    const projects = [
+        {
+            title: "Ecommerce Premium",
+            description: "Plataforma de e-commerce y gestión integrado y personalización de productos.",
+            icon: ShoppingBag,
+            label: "https://github.com/GSUBERO/SastreriaPremium",
+               
+        }
+    ]
     return (
-        <div>
-            <h2>Projects</h2>
+        <div className="py-10">
+            <div className="pb-5">
+                <h2 className="text-3xl md:text-4xl font-bold text-center text-primary-tx text-start">Projects</h2>
+            </div>
+            <section>
+                {projects.map((project, id) => (
+                    <Cards 
+                        key={id} 
+                        {...project}
+                    />
+                ))}
+            </section>
         </div>
     )
 }
