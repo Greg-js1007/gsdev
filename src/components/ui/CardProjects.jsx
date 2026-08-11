130a5a67958e556bb9b87c189cc5e2012745b13f
Gregory Subero <suberog010@gmail.com>
2026-08-10 20:54:38 -0400

    style: agregar espaciados iconos y tamaños

--- src/components/ui/CardProjects.jsx
+++ src/components/ui/CardProjects.jsx
@@ -1,26 +1,26 @@
-import Cards from "./Cards"
+import { ArrowUpRight} from "lucide-react";
+import Cards from "./Cards";
 import Chip from "./Chip";
-import { ArrowBigDown } from "lucide-react";
-const CardProjects = ({date, title, description, skills, link}) => {
-    return( 
-        <Cards>
-            <div className="flex justify-between">
-                <p>{date}</p>
-                <a href={link}>
-                    <ArrowBigDown className="text-primary-tx/50" />
-                </a>
-            </div>
-            <h2>
-                {title}
-            </h2>
-            <p>
-                {description}
-            </p>
-            {skills.map((skill, id) => (
-                <Chip text={skill} key={id} />
-            ))}
-        </Cards>
-    )
-}
+const CardProjects = ({ date, title, description, skills, link }) => {
+  return (
+    <Cards>
+      <div>
+        <div className="flex justify-between">
+          <p className="text-xs">{date}</p>
+          <a href={link}>
+            <ArrowUpRight className="text-primary-tx/50" />
+          </a>
+        </div>
+        <h2 className="text-3xl lg:text-4xl text-primary-tx font-primary font-bold py-2">{title}</h2>
+        <p className="py-2.5 text-sm">{description}</p>
+        <div className="flex gap-2 py-2.5">
+          {skills.map((skill, id) => (
+            <Chip text={skill} key={id} />
+          ))}
+        </div>
+      </div>
+    </Cards>
+  );
+};
 
-export default CardProjects; 
+export default CardProjects;
