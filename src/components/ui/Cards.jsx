b6ebf5d30aeb642418851731d5d223b4a86e1b9a
Gregory Subero <suberog010@gmail.com>
2026-08-12 19:56:29 -0400

    style: max width desde laptop

--- src/components/ui/Cards.jsx
+++ src/components/ui/Cards.jsx
@@ -1,6 +1,6 @@
 const Cards = ({ children}) => {
     return (
-        <div className={`flex flex-col items-start gap-5 border border-white/10 bg-slate-900/40 backdrop-blur-sm rounded-2xl p-5`}>
+        <div className={`flex flex-col items-start gap-5 border border-white/10 bg-slate-900/40 backdrop-blur-sm rounded-2xl p-5 lg:max-w-3xl`}>
             {children}
         </div>
     )
