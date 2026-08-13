6839a478975f1553c01f3271af6e3ed187f2fcc5
Gregory Subero <suberog010@gmail.com>
2026-08-12 20:39:29 -0400

    feat: agregando las props dadas del padre y estilos del border hover

--- src/components/ui/Cards.jsx
+++ src/components/ui/Cards.jsx
@@ -1,6 +1,6 @@
-const Cards = ({ children}) => {
+const Cards = ({ children, borderHover}) => {
     return (
-        <div className={`flex flex-col items-start gap-5 border border-white/10 bg-slate-900/40 backdrop-blur-sm rounded-2xl p-5 lg:max-w-3xl`}>
+        <div className={`flex flex-col items-start gap-5 border border-white/10 bg-slate-900/40 backdrop-blur-sm rounded-2xl p-5 lg:max-w-3xl ${borderHover} transition-all duration-300 ease-in-out`}>
             {children}
         </div>
     )
