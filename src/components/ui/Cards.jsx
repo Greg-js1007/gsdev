f2ae58f9c95ce39e450ac40419ce33a5b1041a24
Gregory Subero <suberog010@gmail.com>
2026-08-12 20:43:47 -0400

    feat: condicioanl para las cards con hover

--- src/components/ui/Cards.jsx
+++ src/components/ui/Cards.jsx
@@ -1,6 +1,7 @@
 const Cards = ({ children, borderHover}) => {
     return (
-        <div className={`flex flex-col items-start gap-5 border border-white/10 bg-slate-900/40 backdrop-blur-sm rounded-2xl p-5 lg:max-w-3xl ${borderHover} transition-all duration-300 ease-in-out`}>
+        <div className={`flex flex-col items-start gap-5 border border-white/10 bg-slate-900/40 backdrop-blur-sm rounded-2xl p-5 lg:max-w-3xl 
+        ${borderHover ? borderHover : "hover:border-secundary-tx"} transition-all duration-300 ease-in-out`}>
             {children}
         </div>
     )
