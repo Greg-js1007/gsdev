89d887049c97effe3b2884642cbcd357e5b4734b
Gregory Subero <suberog010@gmail.com>
2026-08-14 22:36:31 -0400

    feat: agregando card base con formulario y estilos

---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,19 @@
+import Card from "../ui/Cards";
+
+const Contact = () => {
+    return (
+        <section className="pt-20 border-t border-primary-tx/50">
+            <Card>
+                <div>
+                    <h2 className="py-2.5 text-3xl lg:text-5xl font-bold text-primary-tx flex flex-wrap">¿Tienes algo <span className="text-secundary-tx">interesante</span> en mente? Hablemos.</h2>
+                    <p className="text-primary-tx/50 font-primary text-sm">Actualmente abierto a colaboraciones freelance y roles remotos. Cuéntame sobre tu proyecto — respondo en menos de 24 h.</p>
+                </div>
+                <form action="" className="py-10">
+                
+                </form>
+        </Card>
+        </section>       
+    )
+} 
+
+export default Contact; 
