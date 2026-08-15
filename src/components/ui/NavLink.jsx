28ba77670ce67ba77d5414fe6f43c09d03417e71
Gregory Subero <suberog010@gmail.com>
2026-08-14 22:30:39 -0400

    style: agregando flex media querie laptop

--- src/components/ui/NavLink.jsx
+++ src/components/ui/NavLink.jsx
@@ -2,12 +2,12 @@
 
 const NavLink = ({href, children, number}) => {
     return(
-        <ul>
+        <ul className="lg:flex lg:flex-col">
             <li className="border-b border-primary-tx/50 flex items-center gap-2.5">
                 <span className="text-gray-300 text-sm">
                     {number}
                 </span>
-                <a href={href} className="capitalize text-2xl py-2.5 font-semibold text-primary-tx transition-all ease-in-out duration-300 hover:text-secundary-tx focus:text-secundary-tx">{children}</a>
+                <a href={href} className="capitalize text-2xl lg:text-sm py-2.5 font-semibold text-primary-tx transition-all ease-in-out duration-300 hover:text-secundary-tx focus:text-secundary-tx">{children}</a>
             </li>
         </ul>
     )
