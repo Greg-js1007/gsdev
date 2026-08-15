cc4b75dd1b32b9dbef77aaf59cbfb50a0a900ff5
Gregory Subero <suberog010@gmail.com>
2026-08-14 22:29:56 -0400

    feat: añadir Contact al main y quitar navbar

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -2,22 +2,20 @@ import Hero from "../components/sections/Hero";
 import Navbar from "../components/sections/Navbar";
 import TechStack from "../components/sections/TechStack";
 import Projects from "../components/sections/Proyects";
+import Contact from "../components/sections/Contact";
 
 const MainLayout  = ({children}) => {
     return (
         <div className="w-full mx-auto min-w-xs lg:max-w-full">
-            <header>
-                <Navbar />
-            </header>
             <main className="flex flex-col px-5 lg:flex lg:flex-row gap-20 lg:px-20">
                 <Hero />
                 <div className="flex flex-col">
                     <TechStack />
                     <Projects />
+                    <Contact />
                 </div>
             </main>
             <footer className="px-5">
-                {children}
             </footer>
         </div>
     )
