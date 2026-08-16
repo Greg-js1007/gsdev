43743509726215948bd4d86e402d1cbcdfe9eeee
Gregory Subero <suberog010@gmail.com>
2026-08-15 21:43:08 -0400

    feat: agregando inputForm component a Contact con su array de datos mapeados

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,15 +1,52 @@
 import Card from "../ui/Cards";
+import InputForm from "../ui/inputForm";
 
 const Contact = () => {
+
+    const inputs = [
+        {
+            id: 1,
+            name: "nombre",
+            placeholder: "Tu nombre",
+            label: "Nombre",
+            type: "text"
+        },
+        {
+            id: 2,
+            name: "email",
+            placeholder: "Tucorreo@dominio.com",
+            label: "Email",
+            type: "email"
+        },
+        {
+            id: 3,
+            name: "asunto",
+            placeholder: "Colaboración, proyecto, trabajo",
+            label: "Asunto (opcional)",
+            type: "text"
+        },
+        {
+            id: 4,
+            name:"mensaje",
+            placeholder: "Cuéntame sobre tu proyecto...",
+            label: "Mensaje",
+            type: "text"
+        }
+    ]
     return (
-        <section className="pt-20 border-t border-primary-tx/50">
-            <Card>
+        <section className="py-20 border-t border-primary-tx/50">
+            <Card classname = "lg:p-12">
                 <div>
-                    <h2 className="py-2.5 text-3xl lg:text-5xl font-bold text-primary-tx flex flex-wrap">¿Tienes algo <span className="text-secundary-tx">interesante</span> en mente? Hablemos.</h2>
-                    <p className="text-primary-tx/50 font-primary text-sm">Actualmente abierto a colaboraciones freelance y roles remotos. Cuéntame sobre tu proyecto — respondo en menos de 24 h.</p>
+                    <h2 className="py-5 text-3xl lg:text-5xl font-bold text-primary-tx text-start leading-13">¿Tienes algo <br /> <span className="text-secundary-tx"> interesante <br /> </span> en mente?</h2>
+                    <p className="text-primary-tx/50 font-secundary text-md">Actualmente abierto a colaboraciones freelance y roles remotos. <br /> Cuéntame sobre tu proyecto — respondo en menos de 24 h.</p>
                 </div>
-                <form action="" className="py-10">
-                
+                <form action="" className="py-5 grid grid-cols-1 w-full md:grid-cols-2 gap-2.5">
+                    {inputs.map((input) => {
+                    
+                    return(
+                        <InputForm key={input.id} name={input.name} label={input.label} type={input.type} placeholder={input.placeholder} />
+                    )
+                    })}
                 </form>
         </Card>
         </section>       
