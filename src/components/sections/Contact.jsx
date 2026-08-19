0ac5547d88020d70afc93eddaac62336040eccc6
Gregory Subero <suberog010@gmail.com>
2026-08-18 20:14:59 -0400

    feat: agregando react hook form y zod

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,7 +1,18 @@
 import Card from "../ui/Cards";
 import InputForm from "../ui/inputForm";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const schema = z.object({
+    name: z.string().min(1, "Nombre es requerido"),
+    email: z.string().email("Email inválido"),
+    message: z.string().min(1, "Mensaje es requerido"),
+})
 
 const Contact = () => {
+    const { register, handleSubmit, formState: {errors} } = useForm();
+    const onSubmit = (data) => console.log(data);
 
     const inputs = [
         {
@@ -30,7 +41,7 @@ const Contact = () => {
             name:"mensaje",
             placeholder: "Cuéntame sobre tu proyecto...",
             label: "Mensaje",
-            type: "text"
+            type: "textarea"
         }
     ]
     return (
@@ -40,7 +51,7 @@ const Contact = () => {
                     <h2 className="py-5 text-3xl lg:text-5xl font-bold text-primary-tx text-start leading-13">¿Tienes algo <br /> <span className="text-secundary-tx"> interesante <br /> </span> en mente?</h2>
                     <p className="text-primary-tx/50 font-secundary text-md">Actualmente abierto a colaboraciones freelance y roles remotos. <br /> Cuéntame sobre tu proyecto — respondo en menos de 24 h.</p>
                 </div>
-                <form action="" className="py-5 grid grid-cols-1 w-full md:grid-cols-2 gap-2.5">
+                <form action="" onSubmit={handleSubmit(onSubmit)} className="py-5 grid grid-cols-1 w-full md:grid-cols-2 gap-2.5">
                     {inputs.map((input) => {
                     
                     return(
